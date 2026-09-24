094cc3e8b4dfa965679df03e587ae1f4df64be42
dsffsdfsdfdsfsssdfs-a11y <dsffsdfsdfdsfsssdfs@gmail.com>
2026-09-25 02:35:58 +0500

    Always render homepage from current saved content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import HomeClient from './HomeClient';
 import { inlineImage, readContent } from '@/lib/local-content';
 
-export const revalidate = false;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function Home() {
   const content = await readContent();
