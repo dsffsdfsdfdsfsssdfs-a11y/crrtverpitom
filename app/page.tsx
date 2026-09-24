6981f62629dfe4fcdef7e66eadfc1c2df6206b8e
dsffsdfsdfdsfsssdfs-a11y <dsffsdfsdfdsfsssdfs@gmail.com>
2026-09-25 01:40:40 +0500

    Always embed current logo in first page response

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const revalidate = false;
 export default async function Home() {
   const content = await readContent();
   const logoImage = typeof content?.header?.logoImage === 'string' ? content.header.logoImage : '';
-  const inlineLogo = logoImage ? await inlineImage(logoImage) : '';
+  const inlineLogo = logoImage ? await inlineImage(logoImage, 10000000) : '';
 
   return (
     <HomeClient
