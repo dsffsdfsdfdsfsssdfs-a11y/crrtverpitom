c542055f0a630aaae032d4573761ef31df66ef75
dsffsdfsdfdsfsssdfs-a11y <dsffsdfsdfdsfsssdfs@gmail.com>
2026-09-25 22:06:45 +0500

    Allow optimized access to live upload images

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1600, 1920],
     imageSizes: [32, 48, 64, 96, 128, 256, 384],
     qualities: [65, 75],
-    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }]
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'crr-tver.ru', pathname: '/uploads/**' },
+      { protocol: 'https', hostname: 'www.crr-tver.ru', pathname: '/uploads/**' }
+    ]
   },
   async headers() {
     return [
