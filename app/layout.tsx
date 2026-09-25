3b91d0d7a2c4e20b8b2ce1b0cb6b706187a38d8a
dsffsdfsdfdsfsssdfs-a11y <dsffsdfsdfdsfsssdfs@gmail.com>
2026-09-25 20:51:03 +0500

    Performance: optimize app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({children}:{children:React.ReactNode}) {
       <head>
         <link rel="icon" type="image/png" sizes="64x64" href="/favicon-v17.png?v=17" />
         <link rel="apple-touch-icon" href="/favicon-v17.png?v=17" />
-        <link rel="preload" as="image" href="/repo-assets/1790256146854-lyjh500c3dfczzpumdyigwunksyimztdsbhjlv1khcud4udhjd8rer980wfvtjt44dhpo1kcp-jgsej5jfmdaso7.jpg" />
+        <link rel="preload" as="image" href="/hero-optimized.jpg?v=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
